25fa7785591c845b30cf937378a52802f3ab1b79
tej-thambi <tejthambi@gmail.com>
2023-07-12 13:05:02 -0400

    Initial set up
    -Import fonts

---
+++ ticket-app/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <div>
+        <h1 className={styles.header}>bandit</h1>
+      </div>
+    </div>
+  );
+}

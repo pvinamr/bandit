91335c7937bb0bf55758e6b98dabab34a5567216
tej-thambi <tejthambi@gmail.com>
2023-07-14 16:48:49 -0400

    Home page

--- ticket-app/src/pages/home/Home.js
+++ ticket-app/src/pages/home/Home.js
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Home.module.css";
+import SearchResult from "./SearchResult";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      setInputValue(event.target.value);
+      handleSearch();
+    }
+  };
+
+  const handleSearch = () => {
+    // Perform search logic here using the inputValue
+    // For demonstration purposes, I'll just add a dummy search result
+    const newItem = [
+      {
+        name: "Test Name",
+        date: "Test Date",
+        location: "Test Location",
+      },
+      {
+        name: "Test Name 2",
+        date: "Test Date 2",
+        location: "Test Location 2",
+      },
+    ];
+
+    setSearchResults(newItem);
+    setInputValue("");
+  };
+
   return (
     <div className={styles.page}>
       <div>
         <h1 className={styles.header}>bandit</h1>
+        <input
+          type="text"
+          className={styles.inputBox}
+          placeholder="find events"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+        {searchResults.map((result, index) => (
+          <React.Fragment key={index}>
+            <SearchResult
+              name={result.name}
+              date={result.date}
+              location={result.location}
+            />
+            {index !== searchResults.length - 1 && (
+              <div className={styles.resultPadding} />
+            )}
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
---
+++ ticket-app/src/pages/home/SearchResult.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./SearchResult.module.css";
+
+export default function SearchResult({ name, date, location }) {
+  const handleClick = () => {
+    const eventDetails = `${name} | ${date} | ${location}`;
+    alert(eventDetails);
+  };
+
+  return (
+    <div>
+      <button className={styles.searchResult} onClick={handleClick}>
+        <span>{name}</span> | <span>{date}</span> | <span>{location}</span>
+      </button>
+    </div>
+  );
+}
